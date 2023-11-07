ef34ed737b264256e6f2fcd83ddb6fd4e7c76bf3
mitchell <avilesmaicol.08@gmail.com>
2023-11-07 17:01:39 -0500

    appKequi --> processing (25/100%)

--- app/src/main/java/com/wposs/appkequi/LoginActivity.java
+++ app/src/main/java/com/wposs/appkequi/LoginActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.view.animation.RotateAnimation;
@@ -14,23 +15,28 @@ import android.widget.EditText;
 import android.widget.TableLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class LoginActivity extends AppCompatActivity {
-//
+
     private TextView backgroundBlue, backgroundRed,postBackground, login;  private Button entry, createAccount;
     private EditText email, password;
     private TableLayout tableRegister;
 
+    //dataBase
+    private FirebaseFirestore bd;
+    private CollectionReference openBD;
+    private FirebaseAuth auth;//
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);//fullScreen
 
         //layout
         email=findViewById(R.id.editTextEmail);             login = findViewById(R.id.textLogin);                   backgroundBlue = findViewById(R.id.azulLg);
@@ -38,10 +44,40 @@ public class LoginActivity extends AppCompatActivity {
         createAccount=findViewById(R.id.buttonCreateAccount);                                                     postBackground= findViewById(R.id.textViewPostBackground);
         entry=findViewById(R.id.buttonEntry);
 
-        animations();
+        //dataBase on                           animations
+        bd= FirebaseFirestore.getInstance();    animations();
+        openBD=bd.collection("user");
+        auth=FirebaseAuth.getInstance();
+
+
     }
 
     public void entry(View see){
+
+            String thisEmail, thisPassword;
+            thisEmail=email.getText().toString();
+            thisPassword=password.getText().toString();
+
+            if(!thisEmail.isEmpty()&&!thisPassword.isEmpty()){
+
+                openBD.whereEqualTo("email",thisEmail).whereEqualTo("password",thisPassword).get().addOnCompleteListener(task -> {
+
+                   if(task.isSuccessful()){
+                       QuerySnapshot qs=task.getResult();
+
+                       if(qs!=null&&!qs.isEmpty()){
+                           Toast.makeText(LoginActivity.this,"encontrado",Toast.LENGTH_SHORT).show();
+                       }else{
+                           Toast.makeText(LoginActivity.this,"no encontrado",Toast.LENGTH_SHORT).show();
+                       }
+                   }else{
+                       Toast.makeText(LoginActivity.this,"error consulta",Toast.LENGTH_SHORT).show();
+                   }
+                });
+
+            }else{
+                Toast.makeText(this,"Enter the fields",Toast.LENGTH_SHORT).show();
+            }
 
 
     }
--- app/src/main/java/com/wposs/appkequi/RegisterActivity.java
+++ app/src/main/java/com/wposs/appkequi/RegisterActivity.java
@@ -4,65 +4,59 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.util.PatternsCompat;
 import android.content.Intent;
-import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.ProgressBar;
 import android.widget.ScrollView;
-import android.widget.TableLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Pattern;
 
 public class RegisterActivity extends AppCompatActivity {
-    //regularExpressions for password
-    private static final String passwordRegularExpressions = "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}";
 
-    //layout
-    private TextView textRegister, postBackground;
-    private ImageButton buttonBack;                                 private Button buttonValidate;
-    private ScrollView table;
-    private EditText name, lastName, confirmPassword, document;
-    private TextInputLayout email, password;                        private TextInputEditText inEmail, inPassword;
-
-    //bd FireBase
-    private FirebaseAuth bd;        private ProgressBar barra;
+    //variables                                                                                                     layout
+    private TextInputLayout name, lastName, email, password, confirmPassword, numberPhone;                          private TextView textRegister, postBackground;
+    private TextInputEditText inName, inLastName, inEmail, inPassword, inConfirmPassword, inNumberPhone;            private ImageButton buttonBack;     private Button buttonValidate;
+                                                                                                                    private ScrollView table;
+    private FirebaseFirestore bd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        //layout                                                    components                                              colors
-        name=findViewById(R.id.editName);                           textRegister=findViewById(R.id.textRegister);           int error=getResources().getColor(R.color.error);       int basicTint=getResources().getColor(R.color.colorFrom);
-        lastName=findViewById(R.id.editLastName);                   buttonBack=findViewById(R.id.buttonBack);               ColorStateList red=ColorStateList.valueOf(error);       ColorStateList standTint=ColorStateList.valueOf(basicTint);
-        email=findViewById(R.id.editEmail);                         buttonValidate=findViewById(R.id.buttonValidate);       int basicColorHint= getResources().getColor(R.color.colorT2);
-        password=findViewById(R.id.editPassword);                   table=findViewById(R.id.scrollView);                    ColorStateList standColorHint=ColorStateList.valueOf(basicColorHint);
-        inEmail=findViewById(R.id.inEmail);                         postBackground=findViewById(R.id.textPostBackground);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);//fullScreen
+
+        //layout                                                    components
+        name=findViewById(R.id.editName);                           textRegister=findViewById(R.id.textRegister);
+        inName=findViewById(R.id.inName);                           buttonBack=findViewById(R.id.buttonBack);
+        lastName=findViewById(R.id.editLastName);                   buttonValidate=findViewById(R.id.buttonValidate);
+        inLastName=findViewById(R.id.inLastName);                   table=findViewById(R.id.scrollView);
+        email=findViewById(R.id.editEmail);                         postBackground=findViewById(R.id.textPostBackground);
+        inEmail=findViewById(R.id.inEmail);
+        password=findViewById(R.id.editPassword);
         inPassword=findViewById(R.id.inPassword);
-        confirmPassword=findViewById(R.id.editConfirmPassword);     barra=findViewById(R.id.progress);
-        document=findViewById(R.id.editDocument);
+        confirmPassword=findViewById(R.id.editConfirmPassword);
+        inConfirmPassword=findViewById(R.id.inConfirmPassword);
+        numberPhone=findViewById(R.id.editNumberPhone);
+        inNumberPhone=findViewById(R.id.inNumberPhone);
 
-        animations();
-
-        //convert to String
-        String openEmail, openPassword;
-        openEmail=inEmail.getText().toString(); openPassword=inPassword.getText().toString();
+        //OnBD                                  //animations
+        bd=FirebaseFirestore.getInstance();     animations();
 
             inEmail.addTextChangedListener(new TextWatcher() {
 
@@ -73,95 +67,75 @@ public class RegisterActivity extends AppCompatActivity {
 
                 @Override
                 public void onTextChanged(CharSequence s, int start, int before, int count) {
-                    result();
+
                 }
 
                 @Override
                 public void afterTextChanged(Editable s) {
-
-
+                    validationEmail();
                     }
 
             });
+            inPassword.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-    }
-
-    public void validate(){
-        try {
-            //converting layout in string                                                                       colors
-            String tempName, tempLastName, tempEmail, tempPassword, tempConfirmPassword, tempDocument;          int error=getResources().getColor(R.color.error);       int basicTint=getResources().getColor(R.color.colorFrom);
-            tempName=name.getText().toString();                                                                 ColorStateList red=ColorStateList.valueOf(error);       ColorStateList standTint=ColorStateList.valueOf(basicTint);
-            tempLastName=lastName.getText().toString();                                                         int basicColorHint= getResources().getColor(R.color.colorT2);
-            tempEmail=inEmail.getText().toString();                                                               ColorStateList standColorHint=ColorStateList.valueOf(basicColorHint);
-            tempPassword=inPassword.getText().toString();
-            tempConfirmPassword=confirmPassword.getText().toString();
-            tempDocument=document.getText().toString();
-
-            //progressBar
-            barra.setVisibility(View.VISIBLE);
-
-            if(!tempName.isEmpty()&&!tempLastName.isEmpty()&&!tempEmail.isEmpty()&&!tempPassword.isEmpty()&&!tempConfirmPassword.isEmpty()&&!tempDocument.isEmpty()){
-                if (!tempConfirmPassword.contentEquals(tempPassword)) {//alternative equals ignore case()
-
-                    //bd                                goneProgressBar
-                    bd= FirebaseAuth.getInstance();     barra.setVisibility(View.GONE);
-
-                    String validEmail, validPassword;
-                    validEmail=inEmail.getText().toString();
-                    validPassword=inPassword.getText().toString();
-
-                    bd.createUserWithEmailAndPassword(validEmail, validPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                Toast.makeText(RegisterActivity.this, "Account created",Toast.LENGTH_SHORT).show();
-                            } else {
-                                Toast.makeText(RegisterActivity.this, "Authentication failed.",Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    });
-
-
-
-
-
-
-                    confirmPassword.setBackgroundTintList(red);
-                    confirmPassword.setHintTextColor(red);
-                    confirmPassword.setText("");
-
-                    Toast.makeText(this, "the password not is equals, please retry", Toast.LENGTH_SHORT).show();
-                }else {
-                    confirmPassword.setBackgroundTintList(standTint);
-                    confirmPassword.setHintTextColor(standColorHint);
-                    confirmPassword.setHint(R.string.editConfirmPassword);
                 }
 
-                if(tempEmail.contains("@")){
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
 
-                }else{
+                }
 
+                @Override
+                public void afterTextChanged(Editable s) {
+                    validationPassword();
+                }
+            });
+    }
+
+    public void validate(View see){
+        try {
+            //converting layout in string
+            String tempName, tempLastName, tempEmail, tempPassword, tempConfirmPassword, tempNumberPhone;
+            tempName=inName.getText().toString();
+            tempLastName=inLastName.getText().toString();
+            tempEmail=inEmail.getText().toString();
+            tempPassword=inPassword.getText().toString();
+            tempConfirmPassword=inConfirmPassword.getText().toString();
+            tempNumberPhone=inNumberPhone.getText().toString();
+
+
+            if(!tempName.isEmpty()&&!tempLastName.isEmpty()&&!tempEmail.isEmpty()&&!tempPassword.isEmpty()&&!tempConfirmPassword.isEmpty()&&!tempNumberPhone.isEmpty()){
+
+                if (tempConfirmPassword.contentEquals(tempPassword)) {//alternative equals ignore case()
+
+                    putDate(tempName, tempLastName, tempEmail, tempPassword, tempNumberPhone);
+                    Toast.makeText(this, "Loading...", Toast.LENGTH_SHORT).show();
+
+                    //clean errors
+                    inName.setError(null);  inLastName.setError(null);  inEmail.setError(null); inPassword.setError(null);  inConfirmPassword.setError(null);   inNumberPhone.setError(null);
+
+                }else {
+                    //confirmPassword
+                    Toast.makeText(this, "the password not is equals, please retry", Toast.LENGTH_SHORT).show();
+                    confirmPassword.setError("not is equals");
                 }
 
             }else{
+                String error="empty";
                 if(tempName.isEmpty()){
-                    name.setBackgroundTintList(red);
-                    name.setHintTextColor(red);
+                    name.setError(error);
                 }if(tempLastName.isEmpty()){
-                    lastName.setBackgroundTintList(red);
-                    lastName.setHintTextColor(red);
+                    lastName.setError(error);
                 }if(tempEmail.isEmpty()){
-                    email.setBackgroundTintList(red);
-                    email.setHintTextColor(red);
+                    email.setError(error);
                 }if(tempPassword.isEmpty()){
-                    password.setBackgroundTintList(red);
-                    password.setHintTextColor(red);
+                    password.setError(error);
                 }if(tempConfirmPassword.isEmpty()){
-                    confirmPassword.setBackgroundTintList(red);
-                    confirmPassword.setHintTextColor(red);
-                }if(tempDocument.isEmpty()){
-                    document.setBackgroundTintList(red);
-                    document.setHintTextColor(red);
+                    confirmPassword.setError(error);
+                }if(tempNumberPhone.isEmpty()){
+                    numberPhone.setError(error);
                 }
                 Toast.makeText(this,"Characters obligatory",Toast.LENGTH_SHORT).show();
             }
@@ -181,53 +155,29 @@ public class RegisterActivity extends AppCompatActivity {
         finish();
     }
 
-
-    //result of validations
-    private void result(){
-        //return through a boolean
-        boolean v1=validationEmail();
-        boolean v2=validationPassword();
-
-            if(!v1||!v2) {
-                return;
-            }
-        Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show();
-    }
-
     //Validation email and password
-    private boolean validationEmail(){
+    private void validationEmail(){
         //of textInputEditText to string                            multiChar
         String tempEmail=inEmail.getText().toString();              String case1="This site can´t been empty";
                                                                     String case2="Please, enter a valid email";
         if(tempEmail.isEmpty()) {
             email.setError(case1);
-            return false;
-
         }else if(!PatternsCompat.EMAIL_ADDRESS.matcher(tempEmail).matches()) {// compare and return true if there is a match with regular expressions
             email.setError(case2);
-            return false;
-
         }else {
             email.setError(null);
-            return true;
         }
     }
-    private boolean validationPassword(){
+    private void validationPassword(){
         //of textInputEditText to string                                multiChar
         String tempPassword=inPassword.getText().toString();            String case1="This site can´t been empty";
                                                                         String case2="Password is too weak";
-        //class pattern
-        Pattern patron=Pattern.compile(passwordRegularExpressions);
-
         if(tempPassword.isEmpty()) {
             password.setError(case1);
-            return false;
-        }else if(!patron.matcher(tempPassword).matches()/*matches return boolean; says if is equalizer or not*/) {// compare and return true if matches the regular expressions with the passwordRegularExpressions; this case is negation"!"
+        }else if(tempPassword.length()<4) {/*matches return boolean; says if is equalizer or not*/
             password.setError(case2);
-            return false;
         }else {
             password.setError(null);
-            return true;
         }
     }
 
@@ -241,6 +191,30 @@ public class RegisterActivity extends AppCompatActivity {
         buttonValidate.startAnimation(down);
         table.startAnimation(down);
         postBackground.startAnimation(down);
+
+    }
+
+    //send to bd
+    private void putDate(String name, String lastName, String email, String password, String numberPhone){
+
+        Map<String, Object> map= new HashMap<>();
+        map.put("name",name);
+        map.put("lastName", lastName);
+        map.put("email",email);
+        map.put("password",password);
+        map.put("numberPhone",numberPhone);
+
+       bd.collection("user").add(map).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+           @Override
+           public void onSuccess(DocumentReference documentReference) {
+               Toast.makeText(getApplicationContext(),"Saved successfully",Toast.LENGTH_SHORT).show();
+           }
+       }).addOnFailureListener(new OnFailureListener() {
+           @Override
+           public void onFailure(@NonNull Exception e) {
+               Toast.makeText(getApplicationContext(),"Error Database",Toast.LENGTH_SHORT).show();
+           }
+       });
 
     }
 
