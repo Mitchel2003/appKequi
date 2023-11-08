9dee6fa6739969f58642457985535fbb9883de6b
mitchell <avilesmaicol.08@gmail.com>
2023-11-08 17:28:41 -0500

    SQLite (completed)

--- app/src/main/java/com/wposs/appkequi/LoginActivity.java
+++ app/src/main/java/com/wposs/appkequi/LoginActivity.java
@@ -6,7 +6,6 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
